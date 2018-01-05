a658645ce400d62303812406175df3affed85f2b
battlefield1stats <battlefield1stats@gmail.com>
2018-01-06 00:08:24 +0100

    read trn api token, usage of log4j2

--- src/main/java/com/bf1stats/api/BaseStatsController.java
+++ src/main/java/com/bf1stats/api/BaseStatsController.java
@@ -1,5 +1,8 @@
 package com.bf1stats.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,8 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api")
 public class BaseStatsController {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(BaseStatsController.class);
+
+    @Value("${trn.api.token}")
+    private String trnApiToken;
+
     @RequestMapping("baseStats")
     public String baseStats() {
-        return "Hello World!";
+        LOGGER.info(System.getProperty("trn.api.token"));
+        return "Hello World!" + trnApiToken;
     }
 }
--- src/main/java/com/bf1stats/jobs/GetBaseStatJob.java
+++ src/main/java/com/bf1stats/jobs/GetBaseStatJob.java
@@ -2,12 +2,16 @@ package com.bf1stats.jobs;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 public class GetBaseStatJob extends QuartzJobBean {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(GetBaseStatJob.class);
+
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        System.err.println("Triggered."); //TODO implement the call to the battlefieldtracker.com api
+        LOGGER.info("Triggered."); //TODO implement the call to the battlefieldtracker.com api
     }
 }
