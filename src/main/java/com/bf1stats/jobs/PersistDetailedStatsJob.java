74abecc6be6407428f50041b3c8594d3248efcdf
battlefield1stats <battlefield1stats@gmail.com>
2018-01-09 22:04:54 +0100

    get rid of the BasicStatsJson class

--- src/main/java/com/bf1stats/jobs/PersistDetailedStatsJob.java
+++ src/main/java/com/bf1stats/jobs/PersistDetailedStatsJob.java
@@ -5,8 +5,11 @@ import com.bf1stats.client.VehiclesStatsClient;
 import com.bf1stats.client.WeaponsStatsClient;
 import com.bf1stats.dao.DetailedStatsDao;
 import com.bf1stats.domain.db.DetailedStatsDb;
+import com.bf1stats.domain.json.basic.DetailedStatsJson;
 import com.bf1stats.domain.json.basic.DetailedStatsResponseJson;
+import com.bf1stats.domain.json.vehicles.VehiclesJson;
 import com.bf1stats.domain.json.vehicles.VehiclesResponseJson;
+import com.bf1stats.domain.json.weapons.WeaponsJson;
 import com.bf1stats.domain.json.weapons.WeaponsResponseJson;
 import com.bf1stats.transformers.DetailedStatsTransformer;
 import org.quartz.JobExecutionContext;
@@ -14,9 +17,12 @@ import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.*;
 
 public class PersistDetailedStatsJob extends QuartzJobBean {
 
@@ -35,19 +41,52 @@ public class PersistDetailedStatsJob extends QuartzJobBean {
     @Autowired
     private DetailedStatsTransformer detailedStatsTransformer;
 
+    private ExecutorService jobExecutorService;
+
+    private final int maxCount;
+
+    public PersistDetailedStatsJob(@Value("${job.retry.count:5}") int jobRetryCount) {
+        jobExecutorService = newExecutorService();
+        maxCount = jobRetryCount;
+    }
+
     private static Logger LOGGER = LoggerFactory.getLogger(PersistDetailedStatsJob.class);
 
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        LOGGER.info("Going to grab the update snapshot from TRN");
 
-        DetailedStatsResponseJson detailedStatsResponseJson = detailedStatsClient.getFromResource(DetailedStatsResponseJson.class);
-        VehiclesResponseJson vehiclesResponseJson = vehiclesStatsClient.getFromResource(VehiclesResponseJson.class);
-        WeaponsResponseJson weaponsResponseJson = weaponsStatsClient.getFromResource(WeaponsResponseJson.class);
+        LOGGER.info("Going to grab the update snapshot from TRN");
+        if (jobExecutorService.isShutdown()) {
+            jobExecutorService = newExecutorService();
+        }
+        Future<DetailedStatsResponseJson> detailedStatsFuture = jobExecutorService.submit(
+                () -> detailedStatsClient.getFromResource(DetailedStatsResponseJson.class)
+        );
+        Future<VehiclesResponseJson> vehiclesFuture = jobExecutorService.submit(
+                () -> vehiclesStatsClient.getFromResource(VehiclesResponseJson.class)
+        );
+        Future<WeaponsResponseJson> weaponsFuture = jobExecutorService.submit(
+                () -> weaponsStatsClient.getFromResource(WeaponsResponseJson.class)
+        );
+
+        DetailedStatsJson detailedStatsJson;
+        List<VehiclesJson> vehiclesJson;
+        List<WeaponsJson> weaponsJson;
+        try {
+            if (!jobExecutorService.awaitTermination(1L, TimeUnit.MINUTES)) {
+                jobExecutorService.shutdownNow();
+                throw new JobExecutionException("Timeout. Cancel client jobs.", context.getRefireCount() < maxCount);
+            }
+            detailedStatsJson = detailedStatsFuture.get().getResult();
+            vehiclesJson = vehiclesFuture.get().getResult();
+            weaponsJson = weaponsFuture.get().getResult();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new JobExecutionException("Client has failed to get Detailed Stats from API", e, context.getRefireCount() < maxCount);
+        }
 
         LOGGER.info("Converting.");
 
-        DetailedStatsDb detailedStatsDb = detailedStatsTransformer.transform(detailedStatsResponseJson.getResult(), vehiclesResponseJson.getResult(), weaponsResponseJson.getResult());
+        DetailedStatsDb detailedStatsDb = detailedStatsTransformer.transform(detailedStatsJson, vehiclesJson, weaponsJson);
         Optional<DetailedStatsDb> latest = detailedStatsDao.findFirstByOrderByRecordedDesc();
 
         if (!latest.isPresent() || detailedStatsDb.getTimePlayed() > latest.get().getTimePlayed()) {
@@ -58,5 +97,9 @@ public class PersistDetailedStatsJob extends QuartzJobBean {
         }
 
         LOGGER.info("Scheduled job is finished.");
+    }
+
+    private ExecutorService newExecutorService() {
+        return Executors.newFixedThreadPool(3);
     }
 }
